e02cececb3126ee5263887779c10a0ac993e3fb9
yasinehsan <yasinehsan11@gmail.com>
2021-02-25 22:33:32 -0500

    boom

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,15 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: "gatsby-source-google-sheets",
+      options: {
+         spreadsheetId: "1JBGnWte27V1LfuNfIMLht4lflZNt_-kuWwrWITwXrmI",
+         worksheetTitle: "Sheet1",
+         credentials: JSON.parse(`${process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS}`)
+         // credentials: require("./credentials/client_secret.json")
+     }
+   },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,49 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+import Homepage from "../myComponents/homepage"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { get } from "lodash";
 
+const IndexPage = ({data}) => {
+  // const nodes = get(data, "allGoogleSheetSheet1Row.edges", [])
+  //
+  // nodes.map(node =>
+  //   // console.log({...node})
+  //   console.log("boom", node.node.FACTS)
+  // )
+
+  const nodes = get(data, "allGoogleSheetSheet1Row.edges", [])
+
+  nodes.map(node =>
+    // console.log({...node})
+    console.log(node.node.facts)
+  )
+  var fact_arr = []
+
+  for (let i = 0; i < 5; i++){
+    fact_arr.push(
+        <p> facty {i}</p>
+    )
+  }
+  return(
+    <>
+      <Homepage/>
+        <div>{nodes.map(node => <p key={node.node.facts}> {node.node.facts}</p>)}</div>
+    </>
+  )
+}
+
+
+export const query = graphql`
+query {
+  allGoogleSheetSheet1Row {
+   edges {
+     node {
+       facts
+     }
+   }
+ }
+}
+`;
 export default IndexPage
